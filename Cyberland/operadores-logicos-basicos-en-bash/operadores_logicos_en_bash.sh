d8b55e3a716d65dff395612786dd36a6fbaf97d2
Jonathan Ramirez <jonathan176h@gmail.com>
2025-01-10 13:20:52 -0600

    Actualizacion de contenido en archivos de ejercicios de Bash

--- Cyberland/operadores-logicos-basicos-en-bash/operadores_logicos_en_bash.sh
+++ Cyberland/operadores-logicos-basicos-en-bash/operadores_logicos_en_bash.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+echo "Cual es nombre?"
+read name
+echo "ingrese su edad, no se lo diremos a nadie..."
+read -s years
+echo "Gracias por la informacion. :)"
+if [ $years -gt 18 ]; then
+    echo "eres mayor de edad"
+else
+    echo "lo siento, pero no puedes pasar"
+fi
+echo "fin del programa"
